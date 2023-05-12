5a152a209c8764862f0efac4da379846f595d3dc
laura997 <67324199+laura997@users.noreply.github.com>
2023-05-12 17:51:02 +0200

    2 files modified

--- src/lib/fetchTracks.ts
+++ src/lib/fetchTracks.ts
@@ -1,5 +1,19 @@
-const apiToken: string = '';
+import { SavedTrack } from 'spotify-types';
 
-export const fetchTracks = async () => {
-  // This function does nothing, yet
+const apiToken: string =
+  'BQBEb7F0_uDuZhyf2N2xnqrGLvbruk_wBVkYbOBxT4me2K_P3U6Ym6Z3KT14hyyRECas5ZveQg7-ZlJUWCEpc8MJiCv4SA5blXh5BjQM7KEUSz_nWOsBPsfmnVxnQuEf_xpYisO4oynCzQVmvVpCRBKotrMslef2J9fOMg2CNdH2o5VEi72xgwJAK6D0pjl0eyVM0h7zVYrO3fyjdxMWkQQ';
+
+export const fetchTracks = async (): Promise<SavedTrack[]> => {
+  const response = await fetch('https://api.spotify.com/v1/me/tracks', {
+    method: 'GET',
+    headers: {
+      Authorization: 'Bearer ' + apiToken,
+    },
+  });
+  if (!response.ok) {
+    throw new Error(`Fetching tracks failed with status ${response.status}`);
+  }
+  const data = (await response.json()) as { items: SavedTrack[] };
+
+  return data.items;
 };
